35cf358ba53559db12bdef510edd6bf382cbced8
ptsouth97 <ptsouth97@gmail.com>
2021-12-24 13:58:33 -0500

    import csv directly from google sheets

--- traininglog.py
+++ traininglog.py
@@ -7,24 +7,27 @@ import matplotlib.pyplot as plt
 def main():
 	'''main function'''
 
-	filename = 'Training Log - Log.csv'
+	#filename = 'Training Log - Log.csv'
+	sheet_id = '1epQ4axsxFBJ4sTvhTiahAuDX1MqcWfj9iY7igJK5oHA'
+	sheet_name = 'Log'
+	url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
-	df = pd.read_csv(filename, header=1, index_col=0, parse_dates=True)
+	df = pd.read_csv(url, index_col=0, parse_dates=True, header=0)
 	
 	df = df.drop(df.index[0])
-
+	#print(df.columns)
 	single_variable_time_series(df)
 
 
 def single_variable_time_series(df):
 	''' builds a time series plot for a single variable'''
 
-	variable = 'HRV'
+	variable = 'Weight Gurus Weight (pounds)'
 
 	df[variable].plot(marker='.', linewidth=1, color='r')
 	
-	df['SMA_7'] = df[variable].rolling(7).mean()
-	df['SMA_7'].plot()
+	df['SMA_30'] = df[variable].rolling(30).mean()
+	df['SMA_30'].plot(legend=True)
 
 
 	plt.title(variable + ' changes over time')
