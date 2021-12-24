5a399bbca143a4bb0a560ae7c22f9d76e764b96e
ptsouth97 <ptsouth97@gmail.com>
2021-12-24 08:36:43 -0500

    added weight function

--- traininglog.py
+++ traininglog.py
@@ -6,14 +6,25 @@ import matplotlib.pyplot as plt
 
 def main():
 
-	df = pd.read_csv('Training Log - Log.csv', header=1, index_col=0, parse_dates=True)
+	filename = 'Training Log - Log.csv'
+
+	df = pd.read_csv(filename, header=1, index_col=0, parse_dates=True)
 	
 	df = df.drop(df.index[0])
-	#df = df.set_index('Date')
-	print(df)
+
+	weight(df)
+
+
+def weight(df):
 
 	df["Weight (pounds)"].plot()
+
+
+	plt.title('Weight Maintenance')
+	plt.xlabel('Date')
+	plt.ylabel('Weight (pounds)')
 	plt.show()
+
 
 if __name__ == '__main__':
 	main()
