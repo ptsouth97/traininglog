2eeeee61253deda92386ae2d1ad2d85841fa6e90
ptsouth97 <ptsouth97@gmail.com>
2021-12-24 08:25:55 -0500

    dropped first row of df

--- traininglog.py
+++ traininglog.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('Training Log - Log.csv', header=1)
-df = df.drop(0)
-print(df)
+
+def main():
+
+	df = pd.read_csv('Training Log - Log.csv', header=1, index_col=0, parse_dates=True)
+	
+	df = df.drop(df.index[0])
+	#df = df.set_index('Date')
+	print(df)
+
+	df["Weight (pounds)"].plot()
+	plt.show()
+
+if __name__ == '__main__':
+	main()
