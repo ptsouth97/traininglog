c4cc6356ac560aad47f2d95354ab610383bc93e3
ptsouth97 <ptsouth97@gmail.com>
2022-01-02 16:32:50 -0500

    added function to convert pace

--- traininglog.py
+++ traininglog.py
@@ -22,9 +22,9 @@ def main():
 
 	#print(df.columns)
 
-
+	df = convert_pace(df)
 	df = select_run_type(df)
-	single_variable_time_series(df)
+	df = single_variable_time_series(df)
 	
 	#df = filter_dates(df)
 	#two_variable_correlation(df)
@@ -65,8 +65,8 @@ def filter_dates(df):
 
 def single_variable_time_series(df):
 	''' builds a time series plot for a single variable'''
-
-	variable = 'Running speed (mph)'
+	
+	variable = 'Pace (min/mile)'
 
 	abc=df[variable].plot(marker='.', linewidth=1, color='r')
 	
@@ -86,7 +86,7 @@ def single_variable_time_series(df):
 	plt.vlines('2021-5-22', low_end, top_end, colors='orange', linestyle='dashed', label='Sweetgrass half marathon')
 	plt.vlines('2021-1-17', low_end, top_end, colors='black', linestyle='dashed', label='Charleston virtual marathon')
 	plt.legend()
-	plt.savefig(variable+'.png')
+	plt.savefig('Pace.png')
 	plt.show()
 
 	return
@@ -105,7 +105,7 @@ def two_variable_correlation(df):
 	column = df[variable1]
 	upper_end = column.max()
 	lower_end = column.min()
-	print(lower_end)
+	
 	reg = LinearRegression()
 	prediction_space = np.linspace(lower_end, upper_end).reshape(-1,1)
 	X = df[variable1].values.reshape(-1,1)
@@ -127,6 +127,20 @@ def select_run_type(df):
 
 	df = df.loc[df['STRAVA Run type'].isin(['Long run', 'Medium long'])]
 	
+	return(df)
+
+
+def convert_pace(df):
+	''' converts 0:8:00 min/mile to 8.0 min/mile'''
+
+	df['datetime'] = pd.to_datetime(df['Average Pace (min/mile)'])
+	df['minutes'] = df['datetime'].dt.minute
+	df['seconds'] = (df['datetime'].dt.second)/60
+	df['Pace (min/mile)'] = round(df['minutes'] + df['seconds'], 2)
+	#df = df['Pace']
+	#df = df.dropna()
+	#print(df)
+
 	return(df)
 
 
